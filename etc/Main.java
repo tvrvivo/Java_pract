5e594c00ee0ce1ddfbcaeb770f99cc2b3da77c2d
tvrvivo <91936834+tvrvivo@users.noreply.github.com>
2022-04-09 11:07:12 +0300

    Update Main.java

--- etc/Main.java
+++ etc/Main.java
@@ -1,4 +1,11 @@
+/**
+* Print command-line parameters.
+*/
 public class Main {
+  /**
+* Program entry point.
+* @param args command-line parameters list
+*/
 public static void main(String[] args) {
 for (String s: args) {
 System.out.println(s);
